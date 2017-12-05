0155e30a32fb041447fccaa2faf0c965260dc408
Isabelle A <isaanca@nuevaschool.org>
2017-12-05 14:54:12 -0800

    Cleaned up code, added comments, and removed obsolete enemy id feature

--- galaga_ft_tkinter.py
+++ galaga_ft_tkinter.py
@@ -1,8 +1,6 @@
 # To do list: (in no particular order)
 # -Lambdas to remove globals (see comment in 1st project check-in)
-# -Enemies being the ones creating the bullets- seems like a better way to organize
 # -More comments, so people know what I'm doing!
-# -Bullets destroying enemy bullets
 
 import Tkinter # Python graphics library
 import random # for random events
@@ -28,21 +26,38 @@ class Game_Object:
         self.is_alive = True
         self.game_state = True
     
+    # these next two methods are run every frame
     @abc.abstractmethod
     def draw(self, canvas):
+        # draws object on canvas
         pass
     
     @abc.abstractmethod
     def update(self, canvas):
+        # does anything
         pass
     
     def life_check(self):
+        # returns whether or not the object is alive, used for detecting win conditions
         return self.is_alive
     
     def delete(self):
+        # technically doesn't delete to avoid indexing errors (all of the objects are in a
+        # list; it just moves the object offscreen
         self.x = 420
         self.y = 420
         self.is_alive = False
+    
+    def check_collision(self, object_type, target_number):
+        # checks if the object has collided with another
+        for game_object in game_objects:
+            if game_object.__class__.__name__ == object_type:
+                self.object_cors = game_object.get_cors()
+                if math.sqrt((self.x-(self.object_cors[0]))**2 +
+                    (self.y-(self.object_cors[1]+10))**2) <= target_number:
+                    # deletes both objects that have collided
+                    self.delete()
+                    game_object.delete()
     
     @staticmethod
     def win_check():
@@ -50,9 +65,11 @@ class Game_Object:
         global game_state
         alive_counter = 0
         for game_object in game_objects:
+            # if the player is dead you lose the game
             if (game_object.__class__.__name__ == "Player" and
                 game_object.life_check() == False):
                 game_state = "Loss"
+            # if none of the enemies remain alive you win
             elif (game_object.__class__.__name__ == "Enemy" and
                 game_object.life_check() == True):
                 alive_counter += 1
@@ -69,24 +86,15 @@ class Player(Game_Object):
     
     def update(self):
         self.x = x_pos
-        
-        # checks for collisions
-        for game_object in game_objects:
-            if game_object.__class__.__name__ == "Enemy_Bullet":
-                self.bullet_cors = game_object.get_cors()
-                if math.sqrt((self.x-(self.bullet_cors[0]))**2 +
-                    (self.y-(self.bullet_cors[1]+10))**2) <= 15:
-                    self.delete()
+        self.check_collision("Enemy_Bullet", 12)
     
     def draw(self, canvas):
-        canvas.create_polygon(self.x, self.y, self.x-10, self.y+20, self.x+10, self.y+20, 
-            fill="red", outline="")
+        canvas.create_polygon(self.x, self.y-10, self.x-10, self.y+10,
+            self.x+10, self.y+10, fill="red", outline="")
 
 class Enemy(Game_Object):
-    def __init__ (self, x, y, id):
+    def __init__ (self, x, y):
         Game_Object.__init__(self, x, y)
-        self.id = id
-        
         self.speed = 1
         self.counter = 0
     
@@ -98,39 +106,25 @@ class Enemy(Game_Object):
             self.counter = 0
             self.speed = self.speed * -1
         
-        # collision detection
-        for game_object in game_objects:
-            if game_object.__class__.__name__ == "Bullet":
-                self.bullet_cors = game_object.get_cors()
-                if math.sqrt((self.x-(self.bullet_cors[0]))**2 +
-                    (self.y-(self.bullet_cors[1]+10))**2) <= 15:
-                    self.delete()
-                    game_object.delete()
+        self.check_collision("Bullet", 13)
         
-        # each frame the enemy has a 1% chance of shooting a bullet
+        # each frame each enemy has a 1% chance of shooting a bullet
         if random.randint(1, 100) == 100 and self.y != 400:
             game_objects.append(Enemy_Bullet(self.x, self.y))
     
     def draw(self, canvas):
         canvas.create_oval(self.x-10, self.y-10, self.x+10, self.y+10,
-            fill="green", outline="", tags=self.id)
+            fill="green", outline="")
 
 class Bullet(Game_Object):
     def __init__ (self, x, y):
         Game_Object.__init__(self, x, y)
         
-        self.speed = -5
+        self.speed = -3
     
     def update(self):
         self.y += self.speed
-        
-        for game_object in game_objects:
-            if game_object.__class__.__name__ == "Enemy_Bullet":
-                self.bullet_cors = game_object.get_cors()
-                if math.sqrt((self.x-(self.bullet_cors[0]))**2 +
-                    (self.y-(self.bullet_cors[1]+10))**2) <= 6:
-                    self.delete()
-                    game_object.delete()
+        self.check_collision("Enemy_Bullet", 6)
     
     def draw(self, canvas):
         canvas.create_oval(self.x-3, self.y-3, self.x+3, self.y+3,
@@ -151,9 +145,9 @@ def create_player():
     global game_objects
     game_objects.append(Player(200, 350))
 
-def create_enemy(x, y, id):
+def create_enemy(x, y):
     global game_objects
-    game_objects.append(Enemy(x, y, id))
+    game_objects.append(Enemy(x, y))
 
 def create_bullet(x, y):
     global game_objects
@@ -188,12 +182,12 @@ def draw(canvas): # draw loop
 def move_left(event):
     global x_pos
     if x_pos > 20:
-        x_pos -= 20
+        x_pos -= 10
 
 def move_right(event):
     global x_pos
     if x_pos < 380:
-        x_pos += 20
+        x_pos += 10
 
 def player_shoot(event):
     global x_pos
@@ -203,15 +197,18 @@ def player_shoot(event):
             create_bullet(x_pos, 350)
 
 def start_game(event):
+    # runs once the player starts the game
     global game_state
     if game_state == "Waiting":
         game_state = "Writing"
         
-        TEXT_ITEMS = ["a and d keys to move", "press s to shoot", "3", "2", "1", "final buffer item"]
-    
+        # the blank item provides a buffer and gets around some of the weird display stuff
+        TEXT_ITEMS = ["a and d keys to move", "press s to shoot", "3", "2", "1", ""]
+        
         def write_on_canvas(item_number):
             canvas.delete(Tkinter.ALL)
-            canvas.create_text(200, 200, text=TEXT_ITEMS[item_number], fill="white", font=("ubuntu", 24))
+            canvas.create_text(200, 200, text=TEXT_ITEMS[item_number], fill="white",
+                font=("ubuntu", 24))
             if item_number < (len(TEXT_ITEMS)-1):
                 canvas.after(1000, write_on_canvas, item_number+1)
             else:
@@ -237,21 +234,20 @@ if __name__ == '__main__':
     # creates the arrangement of enemies
     pos = 37.5
     for i in range(5):
-        create_enemy(pos, 50, ascii_lowercase[i])
+        create_enemy(pos, 50)
         pos += 75
     
     pos = 75
     for i in range(4):
-        create_enemy(pos, 100, ascii_lowercase[i+5])
+        create_enemy(pos, 100)
         pos += 75
     
     pos = 37.5
     for i in range(5):
-        create_enemy(pos, 150, ascii_lowercase[i+9])
+        create_enemy(pos, 150)
         pos += 75
-    
     game_state = "Waiting"
     canvas.create_text(200, 200, text="click to begin", fill="white",
-                font=("ubuntu", 24))
+        font=("ubuntu", 24))
 
     root.mainloop() # keep the window open
